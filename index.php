6d72b0995f5c9fd8556475c19fec3befb2cb70ed
Jonatan Santos <jonataned13@gmail.com>
2019-04-07 22:00:19 -0300

    Version 0.0.0.2

--- index.php
+++ index.php
@@ -1,245 +1,82 @@
+<?php
 
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="../../../../favicon.ico">
+echo "<pre>";
+print_r($_POST);
+echo "</pre>";
 
-    <title>Cielo Integração</title>
+require 'vendor/autoload.php';
 
-    <!-- Bootstrap core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+use Cielo\API30\Merchant;
 
-    <!-- Custom styles for this template -->
-    <link href="css/form-validation.css" rel="stylesheet">
-  </head>
+use Cielo\API30\Ecommerce\Environment;
+use Cielo\API30\Ecommerce\Sale;
+use Cielo\API30\Ecommerce\CieloEcommerce;
+use Cielo\API30\Ecommerce\Payment;
+use Cielo\API30\Ecommerce\CreditCard;
 
-  <body class="bg-light">
-
-    <div class="container">
-      <div class="py-5 text-center">
-        <img class="d-block mx-auto mb-4" src="img/bootstrap-solid.svg" alt="" width="72" height="72">
-        <h2>Pagamento Online</h2>
-        <p class="lead">Aprenda a criar uma página no seu ecommerce para receber pagamentos online utilizando a API Cielo 3.0 com Bootstrap e PHP.
-Um exemplo rápido e simples, que abre as portas para aplicações mais complexas.</p>
-      </div>
-
-      <div class="row">
-        <div class="col-md-4 order-md-2 mb-4">
-          <h4 class="d-flex justify-content-between align-items-center mb-3">
-            <span class="text-muted">Your cart</span>
-            <span class="badge badge-secondary badge-pill">3</span>
-          </h4>
-          <ul class="list-group mb-3">
-            <li class="list-group-item d-flex justify-content-between lh-condensed">
-              <div>
-                <h6 class="my-0">Nome do Produto</h6>
-                <small class="text-muted">Brief description</small>
-              </div>
-              <span class="text-muted">$12</span>
-            </li>
-            <li class="list-group-item d-flex justify-content-between lh-condensed">
-              <div>
-                <h6 class="my-0">Second product</h6>
-                <small class="text-muted">Brief description</small>
-              </div>
-              <span class="text-muted">$8</span>
-            </li>
-            <li class="list-group-item d-flex justify-content-between lh-condensed">
-              <div>
-                <h6 class="my-0">Third item</h6>
-                <small class="text-muted">Brief description</small>
-              </div>
-              <span class="text-muted">$5</span>
-            </li>
-            <li class="list-group-item d-flex justify-content-between bg-light">
-              <div class="text-success">
-                <h6 class="my-0">Promo code</h6>
-                <small>EXAMPLECODE</small>
-              </div>
-              <span class="text-success">-$5</span>
-            </li>
-            <li class="list-group-item d-flex justify-content-between">
-              <span>Total (USD)</span>
-              <strong>$1</strong>
-            </li>
-          </ul>
-
-          <form class="card p-2">
-            <div class="input-group">
-              <input type="text" class="form-control" placeholder="Promo code">
-              <div class="input-group-append">
-                <button type="submit" class="btn btn-secondary">Redeem</button>
-              </div>
-            </div>
-          </form>
-        </div>
-        <div class="col-md-8 order-md-1">
-          <h4 class="mb-3">Billing address</h4>
-          <form class="needs-validation" novalidate action="efetuar-pagamento.php" method="POST">
-			<input type="hidden" name="total" id="total" value="1">
-            <div class="row">
-              <div class="col-md-6 mb-3">
-                <label for="firstName">First name</label>
-                <input type="text" class="form-control" name="firstName" id="firstName" placeholder="" value="" required>
-                <div class="invalid-feedback">
-                  Valid first name is required.
-                </div>
-              </div>
-              <div class="col-md-6 mb-3">
-                <label for="lastName">Last name</label>
-                <input type="text" class="form-control" name="lastName" id="lastName" placeholder="" value="" required>
-                <div class="invalid-feedback">
-                  Valid last name is required.
-                </div>
-              </div>
-            </div>
-
-           
-
-            <div class="mb-3">
-              <label for="email">Email <span class="text-muted">(Optional)</span></label>
-              <input type="email" class="form-control" name="email" id="email" placeholder="you@example.com">
-              <div class="invalid-feedback">
-                Please enter a valid email address for shipping updates.
-              </div>
-            </div>
-
-            <div class="mb-3">
-              <label for="address">Address</label>
-              <input type="text" class="form-control" name="address" id="address" placeholder="1234 Main St" required>
-              <div class="invalid-feedback">
-                Please enter your shipping address.
-              </div>
-            </div>
-
-            <div class="mb-3">
-              <label for="address2">Address 2 <span class="text-muted">(Optional)</span></label>
-              <input type="text" class="form-control" name="address2" id="address2" placeholder="Apartment or suite">
-            </div>
-
-            <div class="row">
-
-			  
-              <div class="col-md-4 mb-3">
-                <label for="state">State</label>
-                <select class="custom-select d-block w-100" name="state" id="state" required>
-                  <option value="">Choose...</option>
-                  <option>SP</option>
-                </select>
-                <div class="invalid-feedback">
-                  Please provide a valid state.
-                </div>
-              </div>
-			  
-			  
-			<div class="col-md-5 mb-3">
-                <label for="city">City</label>
-                <select class="custom-select d-block w-100" name="city" id="city" required>
-                  <option value="">Choose...</option>
-                  <option>Campinas</option>
-                </select>
-                <div class="invalid-feedback">
-                  Please select a valid city.
-                </div>
-              </div>
-			  
-			  
-			  
-              <div class="col-md-3 mb-3">
-                <label for="zip">Zip</label>
-                <input type="text" class="form-control" id="zip" id="zip" placeholder="" required>
-                <div class="invalid-feedback">
-                  Zip code required.
-                </div>
-              </div>
-            </div>
-            <hr class="mb-4">
-           
-
-            <h4 class="mb-3">Payment</h4>
+use Cielo\API30\Ecommerce\Request\CieloRequestException;
 
 
-            <div class="row">
-              <div class="col-md-6 mb-3">
-                <label for="cc-flag">Flag</label>
-                <input type="text" class="form-control" name="cc-flag" id="cc-flag" placeholder="" required>
-                <div class="invalid-feedback">
-                  Flag is required
-                </div>
-              </div>
-              <div class="col-md-6 mb-3">
-                <label for="cc-number">Credit card number</label>
-                <input type="text" class="form-control" name="cc-number" id="cc-number" placeholder="" required>
-                <div class="invalid-feedback">
-                  Credit card number is required
-                </div>
-              </div>
-            </div>
-            <div class="row">
-              <div class="col-md-3 mb-3">
-                <label for="cc-expiration">Expiration</label>
-                <input type="text" class="form-control" name="cc-expiration" id="cc-expiration" placeholder="" required>
-                <div class="invalid-feedback">
-                  Expiration date required
-                </div>
-              </div>
-              <div class="col-md-3 mb-3">
-                <label for="cc-cvv">CVV</label>
-                <input type="text" class="form-control" name="cc-cvv" id="cc-cvv" placeholder="" required>
-                <div class="invalid-feedback">
-                  Security code required
-                </div>
-              </div>
-            </div>
-            <hr class="mb-4">
-            <button class="btn btn-primary btn-lg btn-block" type="submit">Continue to checkout</button>
-          </form>
-        </div>
-      </div>
 
-      <footer class="my-5 pt-5 text-muted text-center text-small">
-        <p class="mb-1">&copy; 2017-2018 Company Name</p>
-        <ul class="list-inline">
-          <li class="list-inline-item"><a href="#">Privacy</a></li>
-          <li class="list-inline-item"><a href="#">Terms</a></li>
-          <li class="list-inline-item"><a href="#">Support</a></li>
-        </ul>
-      </footer>
-    </div>
+// ...
+// Configure o ambiente
+//::production para o ambiente de produção
+$environment = $environment = Environment::production();
 
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script>window.jQuery || document.write('<script src="js/jquery-slim.min.js"><\/script>')</script>
-    <script src="js/popper.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/holder.min.js"></script>
-    <script>
-      // Example starter JavaScript for disabling form submissions if there are invalid fields
-      (function() {
-        'use strict';
+// Configure seu merchant (credenciais)
+$merchant = new Merchant('e7daa799-e74e-423b-86c5-e05bf5e1e159', '3ZGLtqFU6720L6ym7vhx9Uww4VmBPiDGO9yVGPuE');
 
-        window.addEventListener('load', function() {
-          // Fetch all the forms we want to apply custom Bootstrap validation styles to
-          var forms = document.getElementsByClassName('needs-validation');
+// Crie uma instância de Sale informando o ID do pedido na loja
+$sale = new Sale('123');
 
-          // Loop over them and prevent submission
-          var validation = Array.prototype.filter.call(forms, function(form) {
-            form.addEventListener('submit', function(event) {
-              if (form.checkValidity() === false) {
-                event.preventDefault();
-                event.stopPropagation();
-              }
-              form.classList.add('was-validated');
-            }, false);
-          });
-        }, false);
-      })();
-    </script>
-  </body>
-</html>
+// Crie uma instância de Customer informando o nome do cliente
+$customer = $sale->customer($_POST['firstName'] . ' ' . $_POST['lastName']);
+
+// Crie uma instância de Payment informando o valor do pagamento
+$payment = $sale->payment((int) ($_POST['total'] . '00'));
+
+$payment->setCapture(1);
+
+// Crie uma instância de Credit Card utilizando os dados de teste
+// esses dados estão disponíveis no manual de integração
+$payment->setType(Payment::PAYMENTTYPE_CREDITCARD)
+        ->creditCard($_POST['cc-cvv'], $_POST['cc-flag'])
+        ->setExpirationDate($_POST['cc-expiration'])
+        ->setCardNumber($_POST['cc-number'])  
+        ->setHolder($_POST['firstName'] . ' ' . $_POST['lastName']);
+
+// Crie o pagamento na Cielo
+try {
+    // Configure o SDK com seu merchant e o ambiente apropriado para criar a venda
+    $sale = (new CieloEcommerce($merchant, $environment))->createSale($sale);
+
+    // Com a venda criada na Cielo, já temos o ID do pagamento, TID e demais
+    // dados retornados pela Cielo
+    $paymentId = $sale->getPayment()->getPaymentId();
+
+	echo $sale->getPayment()->getStatus();
+	echo "-";
+	echo $sale->getPayment()->getReturnCode();
+	echo "<pre>";
+	print_r($sale->getPayment());
+	
+	if($sale->getPayment()->getStatus() == 2){
+		Header("Location: retorno.php?cod=0&TID=" . $sale->getPayment()->getTid());
+	}else{
+		Header("Location: retorno.php?cod=1&status=".$sale->getPayment()->getStatus()."&erro=".$sale->getPayment()->getReturnCode());
+	}
+	
+    // Com o ID do pagamento, podemos fazer sua captura, se ela não tiver sido capturada ainda
+    //$sale = (new CieloEcommerce($merchant, $environment))->captureSale($paymentId, 15700, 0);
+
+    // E também podemos fazer seu cancelamento, se for o caso
+    //$sale = (new CieloEcommerce($merchant, $environment))->cancelSale($paymentId, 15700);
+} catch (CieloRequestException $e) {
+    // Em caso de erros de integração, podemos tratar o erro aqui.
+    // os códigos de erro estão todos disponíveis no manual de integração.
+    print_r($e->getCieloError());
+	//$erro = $e->getCieloError()->getMessage() . "-" . $e->getCieloError()->getCode();
+	//echo $erro; die();
+	//echo $e->getCieloError()->code . $e->getCieloError()->message;
+	//Header("Location: retorno.php?cod=2&erro=" . $e->getCieloError()->getCode());
+}
